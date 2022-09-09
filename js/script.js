487a7282ada09ea7c359d014b419132b56a1d14d
ancelottidev <karlo.zovko11@gmail.com>
2022-09-09 04:21:59 +0000

    Wrote function playMatch

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ playBtn.addEventListener('click', () => {
     introScreen.classList.add('unactive');
     match.classList.add('active');
 });
+
+//Play Match Function
+function playMatch(){
+    const options = document.querySelectorAll('.options button');
+    const computerOptions = ['rock', 'paper', 'scissors'];
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
+
+    options.forEach(option => {
+        option.addEventListener('click', function(){
+            const computerNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[computerNumber];
+            
+            setTimeout(() => {
+                //call compare hads function
+                computerHands(this.textContent, computerChoice);
+                
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+            }, 2000);
+
+            //Working on animation
+            playerHand.style.animation = 'shakePlayer 2s ease';
+            computerHand.style.animation = 'shakeComputer 2s ease';
+        });
+    });
+
+    hands.forEach(hand => {
+        hand.addEventListener('animationend', function(){
+            this.style.animation = "";
+        });
+    });
+}
+playMatch();
